b138e143e8f9eced105897199eff508f52fdbd38
Pavel Sushko <nintsao@gmail.com>
2024-10-16 16:16:29 +0300

    added cleanup file fun

--- src/main/kotlin/com/sushkpavel/infrastructure/docker/CppSolutionChecker.kt
+++ src/main/kotlin/com/sushkpavel/infrastructure/docker/CppSolutionChecker.kt
@@ -13,21 +13,22 @@ class CppSolutionChecker(
         val tests = testRepo.getTestByTaskId(solution.taskId)
         saveCodeToFile(solution.code)
         tests?.let {
-                val actualResult = executeTests(tests.inputData)
+            val actualResult = executeTests(tests.inputData)
 
-                if (actualResult != tests.expectedResult) {
-                    return TestResult(
-                        id = it.id,
-                        userId = solution.userId,
-                        solutionId = solution.id,
-                        testId = it.id,
-                        actualResult = "Bad result",
-                        success = false
-                    )
-                }
-
+            if (actualResult != tests.expectedResult) {
+                cleanUpCodeFile()
+                return TestResult(
+                    id = it.id,
+                    userId = solution.userId,
+                    solutionId = solution.id,
+                    testId = it.id,
+                    actualResult = "Bad result",
+                    success = false
+                )
+            }
         }
 
+        cleanUpCodeFile()
         return TestResult(
             id = solution.id,
             userId = solution.userId,
@@ -37,31 +38,49 @@ class CppSolutionChecker(
             success = true
         )
     }
+
     private fun saveCodeToFile(code: String) {
         val file = File("docker/cpp/solution.cpp")
         file.writeText(code)
     }
-//    ktor-leetcode-cpp-compiler
-private fun executeTests(inputData: List<String>): List<String> {
-    val results = mutableListOf<String>()
-    val workingDir = File("docker/cpp").absolutePath
 
-    inputData.forEach { input ->
-        println("input : $input")
-        val process = Runtime.getRuntime().exec(
-            arrayOf("docker", "run", "--rm", "-v", "$workingDir:/usr/src/app", "ktor-leetcode-cpp-compiler", "./run.sh", input)
-        )
-        val output = process.inputStream.bufferedReader().readText().trim()
-        val errorOutput = process.errorStream.bufferedReader().readText().trim()
-
-        if (errorOutput.isNotEmpty()) {
-            println("error : $errorOutput")
+    private fun cleanUpCodeFile() {
+        val file = File("docker/cpp/solution.cpp")
+        if (file.exists()) {
+            file.delete()
         }
-
-        println("output : $output")
-        results.add(output)
     }
 
-    return results
-}
+    //    ktor-leetcode-cpp-compiler
+    private fun executeTests(inputData: List<String>): List<String> {
+        val results = mutableListOf<String>()
+        val workingDir = File("docker/cpp").absolutePath
+
+        inputData.forEach { input ->
+            println("input : $input")
+            val process = Runtime.getRuntime().exec(
+                arrayOf(
+                    "docker",
+                    "run",
+                    "--rm",
+                    "-v",
+                    "$workingDir:/usr/src/app",
+                    "ktor-leetcode-cpp-compiler",
+                    "./run.sh",
+                    input
+                )
+            )
+            val output = process.inputStream.bufferedReader().readText().trim()
+            val errorOutput = process.errorStream.bufferedReader().readText().trim()
+
+            if (errorOutput.isNotEmpty()) {
+                println("error : $errorOutput")
+            }
+
+            println("output : $output")
+            results.add(output)
+        }
+
+        return results
+    }
 }
