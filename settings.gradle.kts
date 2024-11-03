7e9446b36d4489468e506263742bc1f131277ed3
Pavel Sushko <nintsao@gmail.com>
2024-11-03 17:37:04 +0300

    separated services in another mdule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "com.sushkpavel.ktor-leetcode"
-include("compile-service")
-include("check-solution-service")
+include("compiler")
+include("checker")
+include("api")
