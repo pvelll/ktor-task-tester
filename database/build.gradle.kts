6f0a5437fa30242a7db67e418a634334842c0c1f
Pavel Sushko <nintsao@gmail.com>
2025-04-18 12:59:39 +0300

    added db module

---
+++ database/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+}
+
+group = "com.sushkpavel.tasktester"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation(libs.mysql.connector.java)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
